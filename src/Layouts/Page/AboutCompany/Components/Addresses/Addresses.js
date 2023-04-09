2c2fe7ceb2c30a7be08e1bb0c1b2746af4dbfae7
Ejika61 <116801906+Ejika61@users.noreply.github.com>
2023-04-09 18:33:24 +0000

    рамка

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,6 +1,12 @@
 import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
+
+const
+    border-radius: 24px;
+    border: solid #E2E8F0 1px;
+;
+
 function Addresses() {
     return (
         <div className='d-inline-flex flex-column gap-2 mt-4'>
