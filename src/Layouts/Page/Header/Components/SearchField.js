20d3185364681551668736655fe4d27e321ff6b8
Ejika61 <116801906+Ejika61@users.noreply.github.com>
2023-04-09 15:21:19 +0000

    ховер

--- src/Layouts/Page/Header/Components/SearchField.js
+++ src/Layouts/Page/Header/Components/SearchField.js
@@ -15,7 +15,7 @@ const Input = styled.input`
     color: #E2E8F0;
     width: 100%;
     &:hover {
-        background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+        background: #F8FAFC 80%;
     }
     &:focus {
         background: white;
