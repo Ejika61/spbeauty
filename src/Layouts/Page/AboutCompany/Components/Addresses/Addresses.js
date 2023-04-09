440bf1666e138f87c3a61a41116ab508039043dd
Ejika61 <116801906+Ejika61@users.noreply.github.com>
2023-04-09 18:10:58 +0000

    обводка адреса

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -4,6 +4,7 @@ import ListElement from "./ListElement";
 function Addresses() {
     return (
         <div className='d-inline-flex flex-column gap-2 mt-4'>
+            border: solid #E2E8F0 1px;
             <div className='text-rg'>Адреса заведений</div>
             <div className='d-inline-flex flex-column gap-3'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
