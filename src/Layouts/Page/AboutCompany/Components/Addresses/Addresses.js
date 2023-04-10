ccdb82838ce61d7e4361e69f03cf4ae628e80c97
Ejika61 <116801906+Ejika61@users.noreply.github.com>
2023-04-10 09:54:43 +0000

    1

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -5,14 +5,13 @@ function Addresses() {
     return (
         <div className='d-inline-flex flex-column gap-2 mt-4 justify-content-between'>
             <span class="border border-#E2E8F0"></span>
-            <div className='text-rg'>Адреса заведений</div>
             <div className='d-inline-flex flex-column gap-3'>
+                <div className='text-rg'>Адреса заведений</div>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
             </div>
-            <div className='d-inline-flex flex-column'>   
-                <SecondaryButton>Добавить филиал</SecondaryButton>
-                </div>
+            <SecondaryButton>Добавить филиал</SecondaryButton>
+                
         </div>
     );
 }
