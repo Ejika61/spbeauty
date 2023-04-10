466f0ca6a79111f9653d53102a6f0965b64508bc
Ejika61 <116801906+Ejika61@users.noreply.github.com>
2023-04-10 05:52:10 +0000

    1

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -9,6 +9,8 @@ function AboutCompany() {
                 <General/>  {/* Основная информация вроде названия, типа и телефона */}
                 <div className='col'></div>
                 <Addresses/> {/* Список доступных адресов заведений */}
+                    border-radius: 12px;
+                    border: solid #E2E8F0 1px;
                 <div className='col'></div>
             </div>
         </div>
