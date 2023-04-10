8378af41b73cbe7d0b9e003ed324cc230b24f950
Ejika61 <116801906+Ejika61@users.noreply.github.com>
2023-04-10 05:27:24 +0000

    6

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -2,20 +2,19 @@ import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 
-<div>
 
-    function Addresses() {
-        return (
-            <div className='d-inline-flex flex-column gap-2 mt-4'>
-                <div className='text-rg'>Адреса заведений</div>
-                <div className='d-inline-flex flex-column gap-3'>
-                    <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                    <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                    <SecondaryButton>Добавить филиал</SecondaryButton>
-                </div>
+
+function Addresses() {
+    return (
+        <div className='d-inline-flex flex-column gap-2 mt-4'>
+            <div className='text-rg'>Адреса заведений</div>
+            <div className='d-inline-flex flex-column gap-3'>
+                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+                <SecondaryButton>Добавить филиал</SecondaryButton>
             </div>
-        );
-    }
+        </div>
+    );
+}
 
-    export default Addresses;
-    </div>
+export default Addresses;
