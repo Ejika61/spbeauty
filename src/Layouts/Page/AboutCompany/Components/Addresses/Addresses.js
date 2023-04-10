45f9a458926bef8b65814a3052d5ef102e278035
Ejika61 <116801906+Ejika61@users.noreply.github.com>
2023-04-10 05:20:30 +0000

    3

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -5,22 +5,6 @@ import ListElement from "./ListElement";
 
 
 function Addresses() {
-
-    const Input = styled.input`
-    outline: none;
-    border-radius: 24px;
-    border: solid #E2E8F0 1px;
-    background-color: #F8FAFC;
-    color: #E2E8F0;
-    width: 100%;
-    &:hover {
-        background: rgba(248,250,252,0.8);
-    }
-    &:focus {
-        background: #F8FAFC;
-    }
-`;
-
     return (
         <div className='d-inline-flex flex-column gap-2 mt-4'>
             <div className='text-rg'>Адреса заведений</div>
